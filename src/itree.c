0c6ebd1484b5a78b325eb1e40aaf6f05664dcb0e
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-05-30 19:14:17 -0300

    Implementacion

--- src/itree.c
+++ src/itree.c
@@ -1,34 +1,153 @@
 #include "itree.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 ITree itree_crear() {
   return NULL;
 }
 
 void itree_destruir(ITree arbol) {
-
+  if(arbol != NULL){
+    itree_destruir(arbol->left);
+    itree_destruir(arbol->right);
+    free(arbol->intervalo);
+    free(arbol);
+  }
 }
 
-ITree itree_insertar(ITree arbol, Interval datos) {
-
+int itree_altura_aux(ITree raiz){
+  if(raiz == NULL)
+    return 0;
+  int size_izq = itree_altura_aux(raiz->left);
+  int size_der = itree_altura_aux(raiz->right);
+  return size_izq > size_der ? size_izq + 1 : size_der + 1;
 }
 
-ITree itree_eliminar(ITree arbol, Interval datos) {
-
+int itree_altura(ITree raiz){
+  return raiz == NULL ? -1 : itree_altura_aux(raiz) - 1;
 }
 
-INode itree_intersectar(ITree arbol, Interval datos) {
-
+int itree_balance_factor(ITree N){
+  return itree_altura(N->right) - itree_altura(N->left);
 }
 
-void itree_recorrer_dfs(ITree arbol) {
-
+double itree_max_sub(ITree nodo){
+  if(nodo->left == NULL && nodo->right == NULL)
+    return nodo->maySub;
+  else if(nodo->left == NULL)
+    return nodo->right->maySub;
+  else if(nodo->right == NULL)
+    return nodo->left->maySub;
+  return nodo->left->maySub > nodo->right->maySub ? nodo->left->maySub : nodo->right->maySub;
 }
 
-void itree_recorrer_bfs(ITree arbol) {
-
+ITree itree_rotacion_simple_der(ITree raiz){
+  ITree aux = raiz->left;
+  raiz->left = aux->right;
+  aux->right = raiz;
+  if(aux->left != NULL)
+    aux->left->maySub = itree_max_sub(aux->left);
+  aux->right->maySub = itree_max_sub(aux->right);
+  aux->maySub = itree_max_sub(aux);
+  return aux;
 }
+
+ITree itree_rotacion_simple_izq(ITree raiz){
+  ITree aux = raiz->right;
+  raiz->right = aux->left;
+  aux->left = raiz;
+  if(aux->right != NULL)
+    aux->right->maySub = itree_max_sub(aux->right);
+  aux->left->maySub = itree_max_sub(aux->left);
+  aux->maySub = itree_max_sub(aux);
+  return aux;
+}
+
+ITree itree_balancear_izq(ITree raiz){
+  if(itree_balance_factor(raiz->left) < 0){
+    raiz = itree_rotacion_simple_der(raiz);
+  }else{
+    raiz->left = itree_rotacion_simple_izq(raiz->left);
+    raiz = itree_rotacion_simple_der(raiz);
+  }
+  return raiz;
+}
+
+ITree itree_balancear_der(ITree raiz){
+  if(itree_balance_factor(raiz->right) > 0){
+    raiz = itree_rotacion_simple_izq(raiz);
+  }else{
+    raiz->right = itree_rotacion_simple_der(raiz->right);
+    raiz = itree_rotacion_simple_izq(raiz);
+  }
+  return raiz;
+}
+
+ITree itree_insertar(ITree raiz, Interval intervalo) {
+  if(raiz == NULL){
+    raiz = malloc(sizeof(INode));
+    raiz->intervalo = malloc(sizeof(IntervalStruct));
+    raiz->intervalo->bgn = intervalo->bgn;
+    raiz->intervalo->end = intervalo->end;
+    raiz->maySub = intervalo->end;
+    raiz->left = NULL;
+    raiz->right = NULL;
+  }else if(intervalo->bgn < raiz->intervalo->bgn){
+    raiz->left = itree_insertar(raiz->left, intervalo);
+    raiz->maySub = itree_max_sub(raiz);
+    if(itree_balance_factor(raiz) < -1){
+      raiz = itree_balancear_izq(raiz);
+    }
+  }else{
+    raiz->right = itree_insertar(raiz->right, intervalo);
+    if(itree_balance_factor(raiz) > 1){
+      raiz = itree_balancear_der(raiz);
+    }
+  }
+  return raiz;
+}
+
+// ITree itree_eliminar(ITree arbol, Interval intervalo) {
+
+// }
 
 int intersectar(Interval i1, Interval i2) {
-  return (i1.bgn <= i2.end && i2.bgn <= i1.end);
+  return (i1->bgn <= i2->bgn && i2->bgn <= i1->end) || (i1->bgn <= i2->end && i2->end <= i1->end);
+  // return (i1->bgn <= i2->end && i2->bgn <= i1->end) || (i2->bgn <= i1->end && i1->bgn <= i2->end);
 }
+
+// return (i2->bgn >= i1->bgn && i2->bgn <= i1->end) || (i2->end >= i1->bgn && i2->end <= i1->end)
+
+Interval itree_intersectar(ITree arbol, Interval intervalo) {
+  if(arbol != NULL) {
+    if(intersectar(arbol->intervalo, intervalo))
+      return arbol->intervalo;
+    if(arbol->left != NULL && arbol->left->maySub >= intervalo->end)
+      return itree_intersectar(arbol->left, intervalo);
+    return itree_intersectar(arbol->right, intervalo);
+  }
+  return NULL;
+}
+
+// Interval itree_intersectar(ITree arbol, Interval intervalo) {
+//   if(arbol != NULL) {
+//     if(intersectar(arbol->intervalo, intervalo))
+//       return arbol->intervalo;
+//     if(arbol->left != NULL && arbol->left->maySub > )
+//       return itree_intersectar(arbol->left, intervalo);
+//     return itree_intersectar(arbol->right, intervalo);
+//   }
+//   return NULL;
+// }
+
+void itree_recorrer_dfs(ITree arbol) {
+  if(arbol != NULL){
+    printf("[%f %f] Mayor subintervalo: %f\n", arbol->intervalo->bgn, arbol->intervalo->end, arbol->maySub);
+    itree_recorrer_dfs(arbol->left);
+    itree_recorrer_dfs(arbol->right);
+  }
+}
+
+// void itree_recorrer_bfs(ITree arbol) {
+
+// }
--- src/main.c
+++ src/main.c
@@ -3,6 +3,79 @@
 #include "itree.h"
 
 int main(){
+    ITree raiz = itree_crear();
+    
+    Interval intervalo = malloc(sizeof(IntervalStruct));
+
+    intervalo->bgn = 16;
+    intervalo->end = 21;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 8;
+    intervalo->end = 9;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 25;
+    intervalo->end = 30;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 5;
+    intervalo->end = 8;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 15;
+    intervalo->end = 23;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 17;
+    intervalo->end = 19;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 26;
+    intervalo->end = 26;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 0;
+    intervalo->end = 3;
+    raiz = itree_insertar(raiz, intervalo);
+    intervalo->bgn = 6;
+    intervalo->end = 10;
+    raiz = itree_insertar(raiz, intervalo);
+    
+    itree_recorrer_dfs(raiz);
+
+    intervalo->bgn = 0;
+    intervalo->end = 1;
+    // free(intervalo);
+    intervalo = itree_intersectar(raiz, intervalo);
+
+    if(!intervalo) {
+        printf("No hay intersecciones\n");
+    }else {
+        printf("[%f, %f]\n", intervalo->bgn, intervalo->end);
+    }
+    intervalo->bgn = 90;
+    intervalo->end = 90;
+    // free(intervalo);
+    intervalo = itree_intersectar(raiz, intervalo);
+    if(!intervalo) {
+        printf("No hay intersecciones\n");
+    }else {
+        printf("[%f, %f]\n", intervalo->bgn, intervalo->end);
+    }
+    intervalo->bgn = 6;
+    intervalo->end = 6;
+    // free(intervalo);
+    intervalo = itree_intersectar(raiz, intervalo);
+    if(!intervalo) {
+        printf("No hay intersecciones\n");
+    }else {
+        printf("[%f, %f]\n", intervalo->bgn, intervalo->end);
+    }
+    intervalo->bgn = 22;
+    intervalo->end = 22;
+    // free(intervalo);
+    intervalo = itree_intersectar(raiz, intervalo);
+    if(!intervalo) {
+        printf("No hay intersecciones\n");
+    }else {
+        printf("[%f, %f]\n", intervalo->bgn, intervalo->end);
+    }
+
+    itree_destruir(raiz);
 
     return 0;
 }
